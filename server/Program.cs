3a40bef056edee7eca2a28684c088d1a8413531d
YasinStudent <m.yasin.contact@gmail.com>
2025-03-26 14:05:45 +0100

    Added send and receive methods

--- server/Program.cs
+++ server/Program.cs
@@ -7,7 +7,6 @@ using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
-
 using LibData;
 
 // ReceiveFrom();
@@ -30,32 +29,44 @@ public class Setting
     public string? ClientIPAddress { get; set; }
 }
 
-
 class ServerUDP
 {
     static string configFile = @"../Setting.json";
     static string configContent = File.ReadAllText(configFile);
     static Setting? setting = JsonSerializer.Deserialize<Setting>(configContent);
 
+    static EndPoint clientEndpoint = new IPEndPoint(IPAddress.Parse(setting.ClientIPAddress), setting.ClientPortNumber);
+
     // TODO: [Read the JSON file and return the list of DNSRecords]
-
-
-
 
     public static void start()
     {
+        byte[] buffer = new byte[1024];
+        MessageService.clientEndpoint = clientEndpoint;
 
+        Console.WriteLine("Server starting...");
+        Console.WriteLine("");
 
         // TODO: [Create a socket and endpoints and bind it to the server IP address and port number]
+        Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint ServerEndPoint = new IPEndPoint(IPAddress.Parse(setting.ServerIPAddress), setting.ServerPortNumber);
+        ServerSocket.Bind(ServerEndPoint);
+
+        while (true)
+        {
+            // TODO:[Receive and print a received Message from the client] && [Receive and print Hello]
+            Message receivedMessage = MessageService.receiveMessage(ServerSocket, buffer);
+            Console.WriteLine($"[Received Message] type: {receivedMessage.MsgType}, content {receivedMessage.Content}");
+
+            if (receivedMessage.MsgType == MessageType.Hello)
+            {
+                string content = "Hello from client";
+                byte[] sendMessage = MessageService.serializeMessage(MessageType.Welcome, content);
+                MessageService.sendMessage(ServerSocket, sendMessage, MessageType.Welcome, content);
+            }
+        }
 
 
-
-        // TODO:[Receive and print a received Message from the client]
-
-
-
-
-        // TODO:[Receive and print Hello]
 
 
 
@@ -80,6 +91,45 @@ class ServerUDP
         // TODO:[If no further requests receieved send End to the client]
 
     }
+}
 
+public static class MessageService
+{
+    public static EndPoint clientEndpoint = null;
 
+    public static byte[] serializeMessage(MessageType messageType, object content)
+    {
+        Message message = new();
+
+        message.MsgId = 1001;
+        message.MsgType = messageType;
+        message.Content = content;
+
+        byte[] serializedMessage = JsonSerializer.SerializeToUtf8Bytes(message);
+
+        return serializedMessage;
+    }
+
+    public static void sendMessage(Socket ServerSocket, byte[] sendMessage, MessageType type, string content)
+    {
+        ServerSocket.SendTo(sendMessage, 0, sendMessage.Length, SocketFlags.None, clientEndpoint);
+        Console.WriteLine($"[Sended message] Type: {type}, Content: {content}" );
+    }
+
+    public static Message extractMessage(byte[] data)
+    {
+        string receivedMessage = Encoding.ASCII.GetString(data);
+        Message message = JsonSerializer.Deserialize<Message>(receivedMessage);
+
+        return message;
+    }
+
+    public static Message receiveMessage(Socket ServerSocket, byte[] buffer)
+    {
+        int receivedBytes = ServerSocket.ReceiveFrom(buffer, ref clientEndpoint);
+        string data = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+        Message message = JsonSerializer.Deserialize<Message>(data);
+
+        return message;
+    }
 }
